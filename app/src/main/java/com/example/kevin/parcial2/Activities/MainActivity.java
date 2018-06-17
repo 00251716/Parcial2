9b805ed7e34634269ce91b8021eff228facc5112
kevin <0010216@uca.edu.sv>
2018-06-16 23:40:54 -0600

    MainActivity modificada

--- app/src/main/java/com/example/kevin/parcial2/Activities/MainActivity.java
+++ app/src/main/java/com/example/kevin/parcial2/Activities/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity
     private static final String TAG = "MainActivity";
     private Fragment contentFragment;
     private FragmentManager fragmentManager;
-    TextView lblUser, lblToken;
+    TextView lblUser;
 
 
     @Override
@@ -121,7 +121,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void switchContent(Fragment fragment, String tag) {
-        //while (fragmentManager.popBackStackImmediate());
+
 
         if (fragment != null){
             FragmentTransaction transaction = fragmentManager.beginTransaction();
@@ -155,19 +155,16 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.main, menu);
+
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
+
         if (id == R.id.action_settings) {
             SharedData.logOutUser();
             finish();
@@ -190,19 +187,6 @@ public class MainActivity extends AppCompatActivity
                 ;
         }
 
-        /*if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
